47ede860f18486f328ee52ee8d8c33f61e1c86fb
Kriti2004 <kritikiran20@gmail.com>
2023-06-15 11:48:19 +0530

    Done

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+// 2. Function calling/invoking call/invoke
+
+getmynane()
+getMySurname();
+
+// There are two thing related with functions
+
+// 1. Function defination is a one time process
+// Bad Software engineer
+function getmynane(){ // This is not a per the camelCase stander
+    console.log("Kiran")
+}
+function getMySurname(){ // This function name is as per the camelCase standar
+    // Function body
+    // Inside the function body I can write many Statements
+    console.log("Kriti"); //Statement = Instructions
+}
+// 2. Function calling
+getMyAddress("Kashipur, Samastipur");
+            
+// 1. Function Defination
+function getMyAddress(adrr){
+    console.log(adrr);
+}
+
---
+++ js/script2.js
@@ -0,0 +1,15 @@
+// Lets create a function which can add 2 numbers
+
+// 1. Function Defination (one time)
+
+function addTwoNumbers(n1,n2){
+    // Every function retuun something
+    return n1 + n2;
+}
+
+// 2. Function calling (Many times)
+
+    // Associativity Principle
+    // Left = Right
+ var result= addTwoNumbers(10,20);
+ console.log(result);
